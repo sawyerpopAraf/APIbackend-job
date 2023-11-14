b288ee420cf9ff34b1d23b2d901720b703b10ccf
sawyerAraf <sawyerdansestudio@gmail.com>
2023-11-14 21:43:50 +0100

    .2 commit

--- models/Category.js
+++ models/Category.js
@@ -13,7 +13,7 @@ module.exports = (sequelize, Sequelize) => {
 		}
 	);
 	Category.associate = function (models) {
-		Category.belongsTo(models.User, { foreignKey: { allowNull: false } });
+		Category.belongsTo(models.User, { foreignKey: { allowNull: true } });
 	};
 	return Category;
 };
